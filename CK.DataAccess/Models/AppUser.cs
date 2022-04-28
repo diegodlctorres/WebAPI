4e1d762a6a0a7feb7de13cd84e2bc7fdb6d38a7f
Diego de la Cruz <diegodlctorres@gmail.com>
2022-04-28 16:08:34 -0500

    Se usó scaffold para generar las clases de las tablas y se cambió al servidor de prueba

---
+++ CK.DataAccess/Models/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CK.DataAccess.Models
+{
+    public partial class AppUser
+    {
+        public decimal CodFuncionario { get; set; }
+        public decimal Contrasena { get; set; }
+        public bool? Estado { get; set; }
+        public string UsuarioRegistro { get; set; }
+        public string UsuarioActualizacion { get; set; }
+        public DateTime? FechaRegistro { get; set; }
+        public DateTime? FechaActualizacion { get; set; }
+    }
+}
