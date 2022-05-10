bb96755260780ee62a3c6755143493d4576f26f7
Jain0r <maicol.ramos@unmsm.edu.pe>
2022-05-10 12:23:11 -0500

    Controlador Caja (Listar) v 1

--- CK.Services/ConsultarCajaService.cs
+++ CK.Services/ConsultarCajaService.cs
@@ -16,7 +16,6 @@ namespace CK.Services
         {
             this.context = context;
         }
-
         public async Task<object> Listar()
         {
             CottonData db = new CottonData();
@@ -27,7 +26,67 @@ namespace CK.Services
 
             var linea = await db.GetData("CKPRUEBALOGIN", parameters);//Nombre del store
             return linea;
+        }
 
+        public async Task<ResponseService<List<Caja>>> ConsultaCaja(int numCaja)
+        {
+            CottonData db = new CottonData();
+            DbParametro[] parameters = new DbParametro[1];
+            parameters[0] = new DbParametro("p_num_caja", numCaja);
+
+            var linea = await db.GetData("SP_CEL_ALM_SLD_PRD_CONSULTA", parameters, "curSQL");
+            
+            var RespuestaCaja = new ResponseService<List<Caja>>();
+
+            if (linea.GetType().Name.Contains("OracleException"))
+            {
+                RespuestaCaja.Error = linea.ToString();
+                return RespuestaCaja;
+            }
+
+            List<Caja> cajas = mapearCajas(linea);
+
+            RespuestaCaja.Objeto = cajas;
+
+            return RespuestaCaja;
+        }
+
+        public async Task<ResponseService<IEnumerable<T>>> traerCajas<T>(int numCaja)
+        {
+            CottonData db = new CottonData();
+            DbParametro[] parameters = new DbParametro[1];
+            parameters[0] = new DbParametro("p_num_caja", numCaja);
+
+            var linea = await db.GetDataClass<T>("SP_CEL_ALM_SLD_PRD_CONSULTA", parameters, "curSQL");
+
+            var RespuestaCajitas = new ResponseService<IEnumerable<T>>
+            {
+                Objeto = (IEnumerable<T>)linea
+            };
+
+            return RespuestaCajitas;
+            //return (IEnumerable<T>)linea;
+        }
+
+        private List<Caja> mapearCajas(object linea)
+        {
+            IEnumerable<dynamic> aux = (IEnumerable<dynamic>)linea;
+            var AuxCajas = new List<Caja>();
+            foreach (var asd in aux)
+            {
+                var caja = new Caja();
+                caja.DEPOSITO_ENTRADA = asd.DEPOSITO_ENTRADA;
+                caja.DESCRIPCION = asd.DESCRIPCION;
+                caja.DESCRIPCION_DEPOSITO = asd.DESCRIPCION_DEPOSITO;
+                caja.GRUPO = asd.GRUPO;
+                caja.ITEM = asd.ITEM;
+                caja.NIVEL = asd.NIVEL;
+                caja.PEDIDO_VENDA = asd.PEDIDO_VENDA;
+                caja.QTDE_PECAS_REAL = asd.PEDIDO_VENDA;
+                caja.SUB = asd.SUB;
+                AuxCajas.Add(caja);
+            }
+            return AuxCajas;
         }
     }
 }
---
+++ CK.WebAPI/Controllers/CajaController.cs
@@ -0,0 +1,68 @@
+﻿using CK.DataAccess.Models;
+using CK.DataTransferObject;
+using CK.Services;
+using CK.WebAPI.Mappings;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CK.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CajaController : ControllerBase
+    {
+        private readonly IConsultarCajaService service;
+
+        public CajaController(IConsultarCajaService service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet("Listar")]
+        public async Task<object> ListarSPdelMongol()
+        {
+            var resultado = await service.Listar();
+            return resultado;
+        }
+
+        [HttpGet("Consulta")]
+        public async Task<ResponseService<List<CajaDTO>>> ConsultaCaja(int numCaja)
+        {
+                var resultado = await service.ConsultaCaja(numCaja);
+
+            CajaDTO cajas = new CajaDTO();
+
+
+            var resultado2 = new ResponseService<List<CajaDTO>>();
+            //{
+            //    Objeto = resultado.Objeto.ToDataTransferObject()
+            //};
+
+
+
+            if (resultado.Objeto == null)
+            {
+                resultado2.Error = resultado.Error;
+                return resultado2;
+            }
+
+            resultado2.Objeto = resultado.Objeto.ToDataTransferObject();
+            return resultado2;
+
+        }
+
+        [HttpGet("Consulta2")]
+        public async Task<ResponseService<IEnumerable<Caja>>> traerCajas(int numCaja)
+        {
+            CajaDTO cajas = new CajaDTO();
+            var resultado = await service.traerCajas<Caja>(numCaja);
+            //cambiar el responseService<list<cajaz>> a responseService<list<cajadto>>
+            //return resultado;
+            return resultado;
+        }
+
+
+    }
+}
